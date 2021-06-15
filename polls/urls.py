393a4d1778bdf7520412343dbe3161e017fc00dd
factocus <overcome0326@naver.com>
2021-06-15 17:52:09 +0900

    urlpatterns 연습

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # 가장 상위 urls.py 경로 설정에서 ex) 'polls/' 이렇게 작성해서 / 붙일 필요 없는 것
+    path('url_test', views.urlTest, name='url_test')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+# 장고에서 일치하는 패턴을 찾으면, HttpRequest 객체를 첫번째로 인수하고, 경로로 특정 view 함수를 호출함
 from django.http import HttpResponse
 
 # Create your views here.
 
-
+# views
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# views에서 작성한 후, urls.py 파일에 경로 설정만 해주면 됨
+def urlTest(request):
+    return HttpResponse("Url Test")
